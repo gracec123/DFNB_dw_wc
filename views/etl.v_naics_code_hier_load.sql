68c733e14980154cf4877d27413af41ff9776475
Wing Chu <gracecwyu@gmail.com>
2021-03-03 23:06:37 -0700

    Add files

--- views/etl.v_naics_code_hier_load.sql
+++ views/etl.v_naics_code_hier_load.sql
@@ -25,25 +25,21 @@ PURPOSE: Create the etl.v_naics_code_hier_load view
 MODIFICATION LOG:
 Ver      Date        Author        Description
 -----   ----------   -----------   -------------------------------------------------------------------------------
-1.0     11/12/2019   JJAUSSI       1. Built this view for LDS BC IT240
-
-
+1.0     03/03/2021   Grace Chu       1. Built this view for LDS BC IT240
+     
 
 RUNTIME: 
 Approx. 1 sec
-
-NOTES:
-
 
 LICENSE: 
 This code is covered by the GNU General Public License which guarantees end users
 the freedom to run, study, share, and modify the code. This license grants the recipients
 of the code the rights of the Free Software Definition. All derivative work can only be
 distributed under the same license terms.
- 
+
 ******************************************************************************************************************/
 
-     WITH input
+	 WITH input
           AS (SELECT *
                 FROM etl.v_naics_code),
           industry_sector
